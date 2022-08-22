fbfb016a39453e8ec485d4e70356603500b1f372
kickeyhickey <hickeydeveloper@gmail.com>
2022-08-21 17:05:45 -0700

    added clock component + logic

---
+++ src/compoents/Clock.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import './Clock.css'
+
+export default class Clock extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            time: new Date().toLocaleTimeString(),
+        }
+    }
+
+    componentDidMount() {
+        setInterval(() => this.updateClock())
+    }
+
+    updateClock() {
+        this.setState({ time: new Date().toLocaleTimeString()})
+    }
+
+
+  render() {
+    return (
+      
+     <div class='container'>
+        <h1>Local Time:</h1>
+        <h2>{this.state.time}</h2>
+     </div>
+      
+    )
+  }
+}
